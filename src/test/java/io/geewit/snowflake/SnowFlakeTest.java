f38c0da79be368a5b2e86735c677000bc1de2bfc
geewit <hf@geewit.io>
2021-03-31 14:28:07 +0800

    init

--- src/test/java/io/geewit/snowflake/SnowFlakeTest.java
+++ src/test/java/io/geewit/snowflake/SnowFlakeTest.java
@@ -15,9 +15,51 @@ public class SnowFlakeTest {
 
     @Test
     public void cachedIdTest() {
-        for (int i = 0; i < (1 << 12); i++) {
-            long id = SnowFlake.ofCached(1L).getUID();
-            logger.info("cached.id({}): {}", String.format("%1$06d", i), id);
+        for (int i = 0; i < (1 << 2); i++) {
+            long id = SnowFlake.ofCached(48782507661878L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 48782507661878L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofCached(238985134362842L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 238985134362842L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofCached(160979938336661L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 160979938336661L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofCached(258428451323233L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 258428451323233L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofCached(213881050529453L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 213881050529453L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofCached(259897330122789L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 259897330122789L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofCached(116595746306610L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 116595746306610L), String.format("%1$06d", i), id);
             if(ids.contains(id)) {
                 logger.warn("cached.id({}) contains in set", id);
             } else {
@@ -29,11 +71,53 @@ public class SnowFlakeTest {
 
     @Test
     public void defaultIdTest() {
-        for (int i = 0; i < (1 << 12); i++) {
-            long id = SnowFlake.ofDefault(1L).getUID();
-            logger.info("default.id({}): {}", String.format("%1$06d", i), id);
+        for (int i = 0; i < (1 << 2); i++) {
+            long id = SnowFlake.ofDefault(48782507661878L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 48782507661878L), String.format("%1$06d", i), id);
             if(ids.contains(id)) {
-                logger.warn("default.id({}) contains in set", id);
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofDefault(238985134362842L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 238985134362842L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofDefault(160979938336661L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 160979938336661L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofDefault(258428451323233L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 258428451323233L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofDefault(213881050529453L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 213881050529453L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofDefault(259897330122789L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 259897330122789L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
+            } else {
+                ids.add(id);
+            }
+            id = SnowFlake.ofDefault(116595746306610L).getUID();
+            logger.info("cached({}).id({}): {}", String.format("%1$018d", 116595746306610L), String.format("%1$06d", i), id);
+            if(ids.contains(id)) {
+                logger.warn("cached.id({}) contains in set", id);
             } else {
                 ids.add(id);
             }
