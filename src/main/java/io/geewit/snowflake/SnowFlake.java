de9d217a67fd913a2b6b1a813f08e1d83ad0eac6
黄方 <hf@geewit.io>
2024-01-29 14:10:15 +0800

    init

--- src/main/java/io/geewit/snowflake/SnowFlake.java
+++ src/main/java/io/geewit/snowflake/SnowFlake.java
@@ -3,6 +3,9 @@ package io.geewit.snowflake;
 import io.geewit.snowflake.impl.CachedUidGenerator;
 import io.geewit.snowflake.impl.DefaultUidGenerator;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 /**
  * twitter的snowflake算法 -- java实现
@@ -36,5 +39,19 @@ public class SnowFlake {
             }
         }
         return defaultUidGenerator;
+    }
+
+    public static long ofWorkerId() {
+        InetAddress localHost;
+        try {
+            localHost = InetAddress.getLocalHost();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+        String ip = localHost.getHostAddress();
+        String[] parts = ip.split("\\.");
+        int last8Bits = Integer.parseInt(parts[3]);
+        int last16Bits = Integer.parseInt(parts[2]); // shift left 8 bits and combine with the last 8 bits
+        return (((long) last16Bits << 8) | last8Bits) & ((1 << 10) - 1); // shift the first 2 bits left by 8 and combine with the last 8 bits
     }
 }
--- src/main/java/io/geewit/snowflake/impl/DefaultUidGenerator.java
+++ src/main/java/io/geewit/snowflake/impl/DefaultUidGenerator.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -44,18 +45,38 @@ import java.util.concurrent.TimeUnit;
 public class DefaultUidGenerator implements UidGenerator {
     private static final Logger logger = LoggerFactory.getLogger(DefaultUidGenerator.class);
 
+    private static final long EPOCH_MILLS = 1706457600000L;
+
+    private static final long START_MILLS = System.currentTimeMillis();
+
     /**
      * Bits allocate
      */
-    protected int timeBits = 28;
-    protected int workerBits = 22;
-    protected int seqBits = 13;
+    protected int timeBits = 41;
+    protected int workerBits = 10;
+    protected int seqBits = 12;
 
     /**
      * Customer epoch, unit as second. For example 2016-05-20 (ms: 1463673600000)
      */
-    protected String epochStr = "2016-05-20";
-    protected long epochSeconds = TimeUnit.MILLISECONDS.toSeconds(1463673600000L);
+    protected String epochStr = "2024-1-29 00:00:00";
+    private final static Calendar startCalendar = Calendar.getInstance();
+    static {
+        startCalendar.set(65767, Calendar.MARCH, 13, 0, 22, 5);
+    }
+    protected final static long startSeconds = TimeUnit.MILLISECONDS.toSeconds(startCalendar.getTimeInMillis());
+
+
+    private final static Calendar epochCalendar = Calendar.getInstance();
+    static {
+        epochCalendar.set(2024, Calendar.JANUARY, 29, 0, 0, 0);
+    }
+
+    protected long epochSeconds = TimeUnit.MILLISECONDS.toSeconds(epochCalendar.getTimeInMillis());
+
+    private long shiftSeconds() {
+        return startSeconds - epochSeconds;
+    }
 
     /**
      * Stable fields after spring bean initializing
@@ -106,7 +127,7 @@ public class DefaultUidGenerator implements UidGenerator {
         long workerId = (uid << (timestampBits + signBits)) >>> (totalBits - workerIdBits);
         long deltaSeconds = uid >>> (workerIdBits + sequenceBits);
 
-        Date thatTime = new Date(TimeUnit.SECONDS.toMillis(epochSeconds + deltaSeconds));
+        Date thatTime = new Date(TimeUnit.SECONDS.toMillis(epochSeconds + deltaSeconds - this.shiftSeconds()));
         String thatTimeStr = String.format("%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS", thatTime);
 
         // format as string
@@ -145,7 +166,7 @@ public class DefaultUidGenerator implements UidGenerator {
         lastSecond = currentSecond;
 
         // Allocate bits for UID
-        return bitsAllocator.allocate(currentSecond - epochSeconds, workerId, sequence);
+        return bitsAllocator.allocate(currentSecond - epochSeconds + this.shiftSeconds(), workerId, sequence);
     }
 
     /**
@@ -164,7 +185,14 @@ public class DefaultUidGenerator implements UidGenerator {
      * Get current second
      */
     private long getCurrentSecond() {
-        long currentSecond = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+        long currentTimeMillis = System.currentTimeMillis();
+        long currentSecond;
+        if (currentTimeMillis >= EPOCH_MILLS) {
+            currentSecond = epochSeconds + (currentTimeMillis - EPOCH_MILLS) / 1000;
+        } else {
+            currentSecond = EPOCH_MILLS - (currentTimeMillis - START_MILLS) / 1000;
+        }
+
         if (currentSecond - epochSeconds > bitsAllocator.getMaxDeltaSeconds()) {
             throw new UidGenerateException("Timestamp bits is exhausted. Refusing UID generate. Now: " + currentSecond);
         }
@@ -195,7 +223,7 @@ public class DefaultUidGenerator implements UidGenerator {
             this.epochStr = epochStr;
             Date epochDate;
             try {
-                epochDate = DateUtils.parseDate(epochStr,  "yyyy-MM-dd");
+                epochDate = DateUtils.parseDate(epochStr,  "yyyy-MM-dd HH:mm:ss");
                 this.epochSeconds = TimeUnit.MILLISECONDS.toSeconds(epochDate.getTime());
             } catch (ParseException ignored) {
             }
